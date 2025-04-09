21607ce5ffa95b46f275eb3d421f717b651d3341
Kovacs Zsolt <smith.zsolt@gmail.com>
2025-04-10 00:10:26 +0200

    Update package version to 0.3.0 and clean up metadata files
    Incremented the package version from 0.2.0 to 0.3.0 in setup.py and pyproject.toml. Removed unnecessary metadata and build-related files to streamline the repository.

--- build/lib/kovacszsolt_add/__init__.py
+++
@@ -1,5 +0,0 @@
-# kovacszsolt_add/__init__.py
-
-def add(a, b):
-    return a + b
-
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='kovacszsolt_add',
-    version='0.0.1',
+    version='0.3.0',
     author='Te',
     author_email='te@pelda.com',
     description='Egy tesztcsomag GitHub Packages-hez',
