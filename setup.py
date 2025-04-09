ad9d90bc0f86043d21ef4ea5520b3c090f912ed1
Kovacs Zsolt <smith.zsolt@gmail.com>
2025-04-10 00:03:28 +0200

    Első verzió

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='kovacszsolt_add',
+    version='0.0.1',
+    author='Te',
+    author_email='te@pelda.com',
+    description='Egy tesztcsomag GitHub Packages-hez',
+    packages=['kovacszsolt_add'],
+)
